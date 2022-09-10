d54aaec1fabf459f7dfad1d23abef8f4d4a2f2f3
ayyıldız@software <sihlarsefa7@gmail.com>
2022-09-10 19:16:14 +0300

    Add entitiy folder and ıt did database connection

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using E_Trade.MvsWebUI.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,11 @@ namespace E_Trade.MvsWebUI.Controllers
 {
     public class HomeController : Controller
     {
+        DataContext _context = new DataContext();
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            return View(_context.Products.ToList());
         }
         public ActionResult Details()
         {
---
+++ Entity/DataInitializer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace E_Trade.MvsWebUI.Entity
+{
+    public class DataInitializer:DropCreateDatabaseIfModelChanges<DataContext>
+    {
+        protected override void Seed(DataContext context)
+        {
+            var kategoriler = new List<Category>()
+            {
+                new Category(){Name="Shos",Description="Shos idependent"},
+                new Category(){Name="Jeans",Description="jeans idependent"},
+                new Category(){Name="Swith",Description="Swith idependent"}
+            };
+
+            foreach (var kategori in kategoriler)
+            {
+                context.Categories.Add(kategori);
+            }
+            context.SaveChanges();
+
+            var urunler = new List<Product>() {
+
+                new Product(){Name="sarı",Description="",Stock=121,IsApproved=true,CategoryId=1},
+                new Product(){Name="kırmızı",Description="",Stock=121,IsApproved=true,CategoryId=2},
+                new Product(){Name="turuncu",Description="",Stock=121,IsApproved=true,CategoryId=3}
+            };
+
+            foreach ( var urun in urunler)
+            {
+                context.Products.Add(urun);
+            }
+
+            context.SaveChanges();
+            base.Seed(context);
+            
+        }
+    }
+}
